512c2df40e9d12fe48e7487630b09841882d4bf3
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-23 12:07:10 +0800

    new internal method with mixin

--- src/main/java/dev/ryanccn/postmortal_inventory/mixins/LivingEntityMixin.java
+++ src/main/java/dev/ryanccn/postmortal_inventory/mixins/LivingEntityMixin.java
@@ -2,7 +2,6 @@ package dev.ryanccn.postmortal_inventory.mixins;
 
 import dev.ryanccn.postmortal_inventory.PostmortalInventoryMod;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
@@ -16,23 +15,22 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-    @ModifyVariable(method = "tryUseTotem", ordinal = 0, at = @At(value = "LOAD", ordinal = 0))
+    @ModifyVariable(method = "tryUseTotem", index = 2, at = @At(value = "LOAD", ordinal = 0))
     public ItemStack inventoryTotemMixin(ItemStack orig) {
-        PostmortalInventoryMod.LOGGER.info("mixin called");
 
         if (orig == null && ((Object) this) instanceof ServerPlayerEntity player) {
-            PostmortalInventoryMod.LOGGER.info("checking for totems in inventory");
-
             Inventory inv = player.getInventory();
 
             for (int i = 0; i < inv.size(); i++) {
                 ItemStack stk = inv.getStack(i);
-                if (stk.equals(Items.TOTEM_OF_UNDYING)) {
-                    return stk;
+
+                if (stk.isOf(Items.TOTEM_OF_UNDYING)) {
+                    ItemStack stk2 = stk.copy();
+                    stk.decrement(1);
+                    PostmortalInventoryMod.LOGGER.info("Inventory totem used");
+                    return stk2;
                 }
             }
-
-            PostmortalInventoryMod.LOGGER.info("not found");
         }
 
         return orig;
